b1257ab39840898c21725eff1c23cbe45d222f36
smeubank <eubank.steven88@gmail.com>
2022-01-23 10:25:52 +0100

    finish course

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ Sentry.init({
     dsn: "https://a8259c22ffcf4dd2907d84003f4ed971@o673219.ingest.sentry.io/6162608",
     logErrors: true,
     release: __SENTRY_RELEASE__,
+    environment: import.meta.env.MODE, 
     integrations: [
       new Integrations.BrowserTracing({
         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
@@ -21,9 +22,14 @@ Sentry.init({
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.2,
   });
   
 
 app.use(router)
 app.mount('#app')
+const user = {
+    email: "eubank.steven88@gmail.com"
+}
+Sentry.setUser(user);
+Sentry.configureScope((scope) => scope.setUser(null));
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,27 @@
 <script setup>
-  const user = {}
+import * as Sentry from "@sentry/vue";
+
+  const user = {};
+  const post = {
+    id: 1,
+    title: "Hello world",
+    body: "lorem ipsum, blah, blah...",
+  }
+
+  Sentry.setContext("post", {
+    id: post.id,
+    postTitle: post.title,
+  });
+  Sentry.setTag("randomTag", "hello-tag")
+
+  const makeToast = () => {
+    sliceBread();
+  }
 </script>
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
     <button @click="user.email.send()">Send Email</button>
+    <button @click="makeToast">Make Toast</button>
   </div>
 </template>
